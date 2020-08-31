a3a77ca4ba95bd577dc4421cb0fb8627fb02edd0
reZach <reZach@users.noreply.github.com>
2020-08-30 22:25:54 -0500

    Updates

---
+++ precache-manifest.88642c4de47b94424eb6bf6d6681b426.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81dafbb266fe4ea549280f0a485a48c",
+    "url": "/fallguysbuddy/index.html"
+  },
+  {
+    "revision": "0aaff4be204339f0055b",
+    "url": "/fallguysbuddy/static/css/main.fdacdfb0.chunk.css"
+  },
+  {
+    "revision": "a32d524c8da560d2b7e0",
+    "url": "/fallguysbuddy/static/js/2.9c919b64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fallguysbuddy/static/js/2.9c919b64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aaff4be204339f0055b",
+    "url": "/fallguysbuddy/static/js/main.cf2f5fdf.chunk.js"
+  },
+  {
+    "revision": "b114c8a0eda526133199",
+    "url": "/fallguysbuddy/static/js/runtime-main.b8b0d3e1.js"
+  },
+  {
+    "revision": "66c7043b40b3905924fa60cac09d425e",
+    "url": "/fallguysbuddy/static/media/apple.66c7043b.svg"
+  },
+  {
+    "revision": "4ecc7b9d18188caf38af9c7f210aa826",
+    "url": "/fallguysbuddy/static/media/bananas.4ecc7b9d.svg"
+  },
+  {
+    "revision": "8457c8e17467c90260dd2a8d8e83cc05",
+    "url": "/fallguysbuddy/static/media/cherry.8457c8e1.svg"
+  },
+  {
+    "revision": "0dbf486d6693af31ecd2dd512cc782e3",
+    "url": "/fallguysbuddy/static/media/grape.0dbf486d.svg"
+  },
+  {
+    "revision": "7f1ff9f911bd522ce69743af3be7e964",
+    "url": "/fallguysbuddy/static/media/orange.7f1ff9f9.svg"
+  },
+  {
+    "revision": "e57d812f4d23efd9010a826f691ef210",
+    "url": "/fallguysbuddy/static/media/watermelon.e57d812f.svg"
+  }
+]);
